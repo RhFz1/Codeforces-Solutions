12f2dc3780e8dfb27ec624562fd1547964b8ab4e
Noor Mohammed Syed <87633406+RhFz1@users.noreply.github.com>
2022-10-31 21:11:51 +0530

    Add files via upload

---
+++ 829_A.cpp
@@ -0,0 +1,103 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define int            long long int
+#define F              first
+#define S              second
+#define pb             push_back
+#define si             set <int>
+#define vi             vector <int>
+#define pii            pair <int, int>
+#define vpi            vector <pii>
+#define vpp            vector <pair<int, pii>>
+#define mii            map <int, int>
+#define mpi            map <pii, int>
+#define spi            set <pii>
+#define endl           "\n"
+#define sz(x)          ((int) x.size())
+#define all(p)         p.begin(), p.end()
+#define double         long double
+#define que_max        priority_queue <int>
+#define que_min        priority_queue <int, vi, greater<int>>
+#define bug(...)       __f (#__VA_ARGS__, __VA_ARGS__)
+#define print(a)       for(auto x : a) cout << x << " "; cout << endl
+#define print1(a)      for(auto x : a) cout << x.F << " " << x.S << endl
+#define print2(a,x,y)  for(int i = x; i < y; i++) cout<< a[i]<< " "; cout << endl
+
+inline int power(int a, int b)
+{
+	int x = 1;
+	while (b)
+	{
+		if (b & 1) x *= a;
+		a *= a;
+		b >>= 1;
+	}
+	return x;
+}
+
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+
+const int N = 200005;
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	bug(n, m);
+}
+
+int32_t main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt",  "r",  stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+
+	clock_t z = clock();
+
+	int t = 1;
+	cin >> t;
+	while (t--){
+		int n;
+		cin >> n;
+		int qc = 0, ac = 0;
+		string s;
+		cin >> s;
+		vector <int> aftera(n + 1 , 0) , afterq(n + 1 , 0);
+		reverse(s.begin() , s.end());
+		for (int i = 0; i < n; i++){
+			if (s[i] == 'A') {
+				aftera[i + 1] += aftera[i] + 1;
+				afterq[i + 1] = afterq[i];
+			}
+			else {
+				afterq[i + 1] += afterq[i] + 1;
+				aftera[i + 1] = aftera[i];
+			}
+		}
+		bool flag = false;
+		for (int i = n; i >= 1; i--){
+			if (afterq[i] > aftera[i]){
+				flag = true;
+				break;
+			}
+		} 
+		if (flag) cout << "No" << endl;
+		else cout << "Yes" << endl;
+	}
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC);
+
+	return 0;
+}
